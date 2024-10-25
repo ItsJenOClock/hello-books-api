6aacc2790dba9ba5cc7ce508b5c152ec2fb5e919
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-10-25 02:13:20 -0400

    db migration

--- app/models/book.py
+++ app/models/book.py
@@ -1,11 +1,9 @@
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
-    
-books = [
-    Book(1, "Blade Runner", "A sci-fi novel set in a dystopian Los Angeles."),
-    Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
-    Book(3, "It", "A horror novel set in a small town.")
-]
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    author: Mapped[str]
+    isbn: Mapped[int]
