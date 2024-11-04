872608aaee067117d4edfeb93f2c85c17343b8f2
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-11-04 12:13:01 -0500

    added tests

--- tests/test_book_routes.py
+++ tests/test_book_routes.py
@@ -20,3 +20,31 @@ def test_get_one_book(client, two_saved_books):
         "title": "Ocean Book",
         "description": "watr 4evr"
     }
+
+def test_create_one_book_in_empty_db(client):
+    response = client.post("/books", json={
+        "title": "Romeo and Juliet",
+        "description": "tragic love story",
+    })
+    response_body = response.get_json()
+    
+    assert response.status_code == 201
+    assert response_body == {
+        "id": 1,
+        "title": "Romeo and Juliet",
+        "description": "tragic love story",
+    }
+
+def test_create_one_book_with_planets_already_in_db(client, two_saved_books):
+    response = client.post("/books", json={
+        "title": "Romeo and Juliet",
+        "description": "tragic love story",
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 201
+    assert response_body == {
+        "id": 3,
+        "title": "Romeo and Juliet",
+        "description": "tragic love story",
+    }
