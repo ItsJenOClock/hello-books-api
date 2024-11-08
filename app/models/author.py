2e4cd1b306a65916f99b21e015c7a0eccc9498ed
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-11-08 06:49:05 -0500

    updated author and book models with relationships to each other

--- app/models/author.py
+++ app/models/author.py
@@ -1,19 +1,31 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
+from typing import Optional
 from ..db import db
 
-class Author(db.Model):
+class Book(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str]
-    books: Mapped[list["Book"]] = relationship(back_populates="author")
-
+    title: Mapped[str]
+    description: Mapped[str]
+    author_id: Mapped[Optional[int]] = mapped_column(ForeignKey("author.id"))
+    author: Mapped[Optional["Author"]] = relationship(back_populates="books")
+    
     def to_dict(self):
-        author_as_dict = {
-            "id": self.id,
-            "name": self.name
-        }
-        return author_as_dict
+        book_as_dict = {}
+        book_as_dict["id"] = self.id
+        book_as_dict["title"] = self.title
+        book_as_dict["description"] = self.description
+        if self.author:
+            book_as_dict["author"] = self.author.name
+        return book_as_dict
     
     @classmethod
-    def from_dict(cls, author_data):
-        new_author = cls(name=author_data["name"])
-        return new_author
+    def from_dict(cls, book_data):
+        author_id = book_data.get("author_id")
+
+        new_book = cls(
+            title=book_data["title"],
+            description=book_data["description"],
+            author_id=author_id
+        )
+        return new_book
--- app/models/book.py
+++ app/models/book.py
@@ -9,26 +9,22 @@ class Book(db.Model):
     description: Mapped[str]
     author_id: Mapped[Optional[int]] = mapped_column(ForeignKey("author.id"))
     author: Mapped[Optional["Author"]] = relationship(back_populates="books")
-
+    
     def to_dict(self):
         book_as_dict = {}
         book_as_dict["id"] = self.id
         book_as_dict["title"] = self.title
         book_as_dict["description"] = self.description
-
         if self.author:
             book_as_dict["author"] = self.author.name
-
         return book_as_dict
     
     @classmethod
     def from_dict(cls, book_data):
         author_id = book_data.get("author_id")
-
         new_book = cls(
             title=book_data["title"],
             description=book_data["description"],
             author_id=author_id
         )
-
         return new_book
